2dc0462e50e412e114de2e354eec47fe4e7279be
Luca Spolladore <luca.spolladore@outlook.it>
2024-11-01 18:07:19 +0100

    moved email template outside chains.py

--- chains.py
+++ chains.py
@@ -5,6 +5,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
+from prompts import email_template, extraction_template
 
 
 class Chain:
@@ -44,23 +45,7 @@ class Chain:
     def write_email(self, job: str, links: List[str]) -> str:
         """Chain to write email from job description and relevant corporate links"""
         prompt_email = PromptTemplate.from_template(
-            """
-            ### JOB DESCRIPTION:
-            {job_description}
-
-            ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
-            the seamless integration of business processes through automated tools. 
-            Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
-            process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
-            in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
-            Do not provide a preamble.
-            ### EMAIL (NO PREAMBLE):
-
-            """
+            email_template
         )
         chain_email = prompt_email | self.llm
         res = chain_email.invoke(
