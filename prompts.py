941d0fba2f6bd756d662fd63097cb7c7fbfd07f7
Luca Spolladore <97733529+SpollaL@users.noreply.github.com>
2024-11-03 09:57:47 +0100

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -4,14 +4,14 @@ email_template = """
     {job_description}
 
     ### INSTRUCTION:
-    You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
+    You are Mohan, a business development executive at Aflight. Aflight is an AI & Software Consulting company dedicated to facilitating
     the seamless integration of business processes through automated tools. 
     Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
     process optimization, cost reduction, and heightened overall efficiency. 
-    Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+    Your job is to write a cold email to the client regarding the job mentioned above describing the capability of Aflight 
     in fulfilling their needs.
-    Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-    Remember you are Mohan, BDE at AtliQ. 
+    Also add the most relevant ones from the following links to showcase Aflight's portfolio: {link_list}
+    Remember you are Mohan, BDE at Aflight. 
     Do not provide a preamble.
     ### EMAIL (NO PREAMBLE):
 
